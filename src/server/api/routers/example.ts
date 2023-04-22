14fddb9f9470f8243d8b8506bdb16b0c8a92c3fe
Berwyn Tan <berwyntan@gmail.com>
2023-04-22 17:27:25 +0800

    Add T3 stack and connected MongoDB

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.example.findMany()
+    console.log(posts)
+    return posts;
+  }),
+});
